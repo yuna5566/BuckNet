c5b8abe8b481083bc830265227d3fa9a71753e2a
michael <yuna5566@gmail.com>
2020-01-23 04:15:37 +0800

    Initial commit for Login.js. Added Facebook SDK for Login Access. FB-SDK is OAuth2.0 Compliant. However, not sure with OpenID

--- app/Containers/Login.js
+++ app/Containers/Login.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
 const Login = props => {
     return(
         <View>
-            <Text>
-                Login
-            </Text>
+            <Text> Login </Text>
+            <View>
+                <LoginButton
+                onLoginFinished={
+                    (error, result) => {
+                        if (error) {
+                            console.log("login has error: " + result.error);
+                        } else if (result.isCancelled) {
+                            console.log("login is cancelled.");
+                        } else {
+                            AccessToken.getCurrentAccessToken().then(
+                                (data) => {
+                                    console.log(data.accessToken.toString())
+                                }
+                            )
+                        }
+                    }
+                }
+                onLogoutFinished={() => console.log("logout.")}/>
+            </View>
         </View>
     )
 }

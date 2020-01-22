e8a7af2b1ae66f175eaff5a3f2f156692ad0faa1
michael <yuna5566@gmail.com>
2020-01-23 02:52:29 +0800

    Initial commit for changes in AuthLoading.js file.

--- app/Containers/AuthLoading.js
+++ app/Containers/AuthLoading.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import {Text, StyleSheet, SafeAreaView, ActivityIndicator} from 'react-native'
+import {Text, StyleSheet, SafeAreaView, ActivityIndicator, StatusBar} from 'react-native'
 
 
 
 const AuthLoading = props => {
     const continueForNow = () => {
         setTimeout(() => {
-            props.navigation.navigate("Home")
+            props.navigation.navigate("Login")
         }, 2000)
     }
 
     continueForNow()
     return(
         <SafeAreaView style={styles.container}>
+            <StatusBar backgroundColor="#B19CD9"/>
             <Text style={styles.title}>BUCKNET</Text>
-            <ActivityIndicator size="large" color="violet"/>
-            <Text>Some Description</Text>
+            <ActivityIndicator size="large" color="#FDFD96"/>
+            <Text>Let them help yours, by sharing it to them!</Text>
         </SafeAreaView>
     )
 }
@@ -24,7 +25,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#B19CD9'
     },
     title: {
         fontSize: 20,
